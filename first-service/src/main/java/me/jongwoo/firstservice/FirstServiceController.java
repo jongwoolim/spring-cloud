15de5ded6ef053fb131b8c84e2d9a7bbb4739605
limjongwoo <whddn528@naver.com>
2021-07-04 20:39:38 +0900

    Spring cloud Gateway - Filter add

--- first-service/src/main/java/me/jongwoo/firstservice/FirstServiceController.java
+++ first-service/src/main/java/me/jongwoo/firstservice/FirstServiceController.java
@@ -1,6 +1,8 @@
 package me.jongwoo.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 // http://localhost:8081/first-service/welcome
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/welcome")
@@ -15,5 +18,11 @@ public class FirstServiceController {
 
         return "Welcome to the First service";
 
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello world in First Service";
     }
 }
