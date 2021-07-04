51be1549fe002acd61db654de5f017f6090bfe96
limjongwoo <whddn528@naver.com>
2021-07-04 20:13:26 +0900

    eureka,zuul,apigateway service discovery

---
+++ second-service/src/main/java/me/jongwoo/secondservice/SecondServiceController.java
@@ -0,0 +1,16 @@
+package me.jongwoo.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+
+        return "welcome to the Second Service";
+    }
+}
