902c7ad72830a2c0bcab41d0dc83e041614d19fc
limjongwoo <whddn528@naver.com>
2021-07-18 20:56:42 +0900

    Rest Template LoadBalanced

--- user-service/src/main/java/me/jongwoo/userservice/config/AppConfig.java
+++ user-service/src/main/java/me/jongwoo/userservice/config/AppConfig.java
@@ -3,6 +3,7 @@ package me.jongwoo.userservice.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +26,7 @@ public class AppConfig {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
