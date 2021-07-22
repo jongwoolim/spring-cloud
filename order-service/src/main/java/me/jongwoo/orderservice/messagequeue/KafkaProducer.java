d178162370d33a77f832427a9ec53291204cdab1
limjongwoo <whddn528@naver.com>
2021-07-22 22:05:08 +0900

    Kafka Producer Config

---
+++ order-service/src/main/java/me/jongwoo/orderservice/messagequeue/KafkaProducer.java
@@ -0,0 +1,32 @@
+package me.jongwoo.orderservice.messagequeue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.jongwoo.orderservice.dto.OrderDto;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public OrderDto send(String topic, OrderDto orderDto){
+        String jsonInString = "";
+        try{
+            jsonInString = objectMapper.writeValueAsString(orderDto);
+        }catch(JsonProcessingException ex){
+            ex.printStackTrace();
+        }
+
+        kafkaTemplate.send(topic, jsonInString);
+        log.info("Kafka Producer sent data from the Order microservice: " + orderDto);
+
+        return orderDto;
+    }
+}
